c4e4ada1385bb12fa1582d1aa48a53eff321b5f1
Orivaldo Santana <orivaldo@gmail.com>
2022-06-15 14:51:53 -0300

    presença discord

--- codigos/principal_forms.py
+++ codigos/principal_forms.py
@@ -6,19 +6,19 @@ colunas_u = []
 # 0 - I
 # 1 - II
 # 2 - III 
-indiceU = 0
+indiceU = 2
 
 #caminho_dir = "~/projetos_ml/presenca_formularios/"
 
 import numpy as np 
 nomes_arqs_forms_u.append( [ 'Compreensão do Perfil da Turma', 'História dos computadores', 'Algoritmo', 'Algoritmos Representação', 'Operadores Aritméticos', 'Operadores Lógicos', 'Estrutura Condicional', 'Estrutura Condicional Aplicações','Sensação de Aprendizado I' ] )
-nomes_arqs_forms_u.append( [ 'Repetição Condicional', 'Repetição Condicional Aplicações', 'Introdução ao P5JS', 'Mouse, teclado e ponto dentro de um retângulo', 'Repetição Condicional e Introdução a Laço Contado', 'Repetição Contada'] )
+nomes_arqs_forms_u.append( [ 'Repetição Condicional', 'Repetição Condicional Aplicações', 'Introdução ao P5JS', 'Mouse, teclado e ponto dentro de um retângulo', 'Repetição Condicional e Introdução a Laço Contado', 'Repetição Contada','Sensação de Aprendizado II'] )
 nomes_arqs_forms_u.append( [ 'Introdução e Manipulação de Vetores','Uso e Exemplos de Vetores'] )
 
 # Colunas abreviadas para a primeira unidade 
 #colunas_u_1 = ['perfil', 'hist','alg','alg_rep','op_ari','op_log','cond','cond_ap']
 colunas_u.append( ['perfil', 'hist','alg','alg_rep','op_ari','op_log','cond','cond_ap', 'sens_ap'] )
-colunas_u.append( [ 'rep','rep_ap','intro_p5js', 'mouse','rep_cont_1','rep_cont_2'] )
+colunas_u.append( [ 'rep','rep_ap','intro_p5js', 'mouse','rep_cont_1','rep_cont_2','sens_ap_2'] )
 colunas_u.append( ['vet_1','vet_2'] )
 
 import pandas as pd 
@@ -29,8 +29,8 @@ import pandas as pd
 #turma_sigaa = pd.read_csv("../dados/turma_4_2020_6.csv")
 #turma_sigaa = pd.read_csv("../dados/turma_03_2020_2.csv")
 #turma_sigaa = pd.read_csv("../dados/turma_02_2021_1.csv")
-#turma_sigaa = pd.read_csv("../dados/turma_01_2021_2.csv")
-turma_sigaa = pd.read_csv("../dados/teste.csv")
+turma_sigaa = pd.read_csv("../dados/turma_01_2021_2.csv")
+#turma_sigaa = pd.read_csv("../dados/teste.csv")
 
 
 mats_sigaa = turma_sigaa.iloc[:,0].values
@@ -39,6 +39,6 @@ from processa_forms import gera_dados_presenca
 
 # Gera presença para as planilhas da unidade 3
 dados_frequencia =  gera_dados_presenca(nomes_arqs_forms_u[indiceU],colunas_u[indiceU],mats_sigaa)
-dados_frequencia.head() 
+print( dados_frequencia.head()  )
 
 dados_frequencia.to_csv("../saida/froms_u{unidade}.csv".format(unidade=indiceU+1), index=False)
